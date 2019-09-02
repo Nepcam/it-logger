96fdf16dae3bda13376ff0fbe6c93995f968b65b
Nepcam <cameron.nepe@gmail.com>
2019-09-03 09:06:19 +1200

    fixed a few errors - attention to detail needed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Fragment, useEffect} from 'react';
 import SearchBar from './components/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
-import AddLogModal from './components/logs/AddLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
